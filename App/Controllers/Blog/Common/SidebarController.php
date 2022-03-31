03ec5127c1fb73a1660bf91bbc7ac8c22ecd6d43
= <=>
2022-03-31 13:14:27 +0100

    [IMP] remove sidebar

--- App/Controllers/Blog/Common/SidebarController.php
+++ App/Controllers/Blog/Common/SidebarController.php
@@ -8,7 +8,7 @@ class SidebarController extends Controller
 {
     public function index()
     {
-      
+        $data =[];
         return $this->view->render('blog/common/sidebar', $data);
     }
 }
