15a3a973d119d02c384e3c9c26768a2d044c9c08
= <=>
2022-03-31 11:07:33 +0100

    [IMP] add index function to show registration page

--- App/Controllers/RegisterController.php
+++ App/Controllers/RegisterController.php
@@ -6,5 +6,17 @@ use System\Controller;
 
 class RegisterController extends Controller
 {
-   
+     /**
+     * Display Registration Page
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        $this->blogLayout->title('Create New Account');
+        $view = $this->view->render('blog/users/register');
+        $this->blogLayout->disable('sidebar');
+
+        return $this->blogLayout->render($view);
+    }
 }
