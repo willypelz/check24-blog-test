dfa0d78f08f537a13de03edb6f27abc11b892cfb
= <=>
2022-03-31 12:22:41 +0100

    [IMP] update sidebar partial

--- App/Controllers/Blog/Common/SidebarController.php
+++ App/Controllers/Blog/Common/SidebarController.php
@@ -8,10 +8,7 @@ class SidebarController extends Controller
 {
     public function index()
     {
-        $data['categories'] = $this->load->model('Categories')->getEnabledCategoriesWithNumberOfTotalPosts();
-
-        $data['ads'] = $this->load->model('Ads')->enabled();
-
+      
         return $this->view->render('blog/common/sidebar', $data);
     }
 }
