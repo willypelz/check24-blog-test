2c2a89e7f47e151d5e478eca4346273b166cd3dd
= <=>
2022-03-31 11:40:16 +0100

    [IMP] add method to display all the login page

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -6,5 +6,24 @@ use System\Controller;
 
 class LoginController extends Controller
 {
-    
+    /**
+    * Show Login Form Method
+    *
+    * @return mixed
+    */
+    public function index()
+    {
+        $this->blogLayout->title('Login');
+
+        $loginModel = $this->load->model('Login');
+        $this->blogLayout->disable('sidebar');
+        if ($loginModel->isLogged()) {
+            return $this->url->redirectTo('/');
+        }
+        $data['errors'] = $this->errors;
+        $view = $this->view->render('blog/users/login', $data);
+        return $this->blogLayout->render($view);
+    }
+
+
 }
