fc8cf6af47a8b00052e9da44ef604d47cc272f6e
= <=>
2022-03-31 10:49:57 +0100

    [IMP] update white spaces

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 date_default_timezone_set('Africa/Cairo');
-
 require __DIR__ . '/vendor/System/Application.php';
 require __DIR__ . '/vendor/System/File.php';
 
@@ -9,7 +8,5 @@ use System\File;
 use System\Application;
 
 $file = new File(__DIR__);
-
 $app = Application::getInstance($file);
-
 $app->run();                        
