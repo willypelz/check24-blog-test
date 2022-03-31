9daf02c929c4ff37b960fdff60a651bedad7985c
= <=>
2022-03-31 10:52:29 +0100

    [IMP] update white spaces

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-date_default_timezone_set('Africa/Cairo');
+date_default_timezone_set('Africa/Lagos');
 require __DIR__ . '/vendor/System/Application.php';
 require __DIR__ . '/vendor/System/File.php';
 
